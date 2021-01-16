39c11ca4a359800f2fa0d9ba1050a7bdce35dff6
GuoYu Yang <49673553+pilgrimygy@users.noreply.github.com>
2021-01-16 14:02:10 +0800

    fix bug

--- src/planner.jl
+++ src/planner.jl
@@ -152,14 +152,45 @@ function backup!(D::DESPOT, b::Int, p::PL_DESPOTPlanner)
         ba = D.parent[b]
         b = D.parent_b[b]
 
+        # https://github.com/JuliaLang/julia/issues/19398
+        #=
         D.ba_mu[ba] = D.ba_rho[ba] + sum(D.mu[bp] for bp in D.ba_children[ba])
-        D.ba_l[ba] = D.ba_rho[ba] + sum(D.l[bp] for bp in D.ba_children[ba])
+        =#
+        sum_mu = 0.0
+        for bp in D.ba_children[ba]
+            sum_mu += D.mu[bp]
+        end
+        D.ba_mu[ba] = D.ba_rho[ba] + sum_mu
 
-        D.U[b] = maximum(D.ba_Rsum[ba] * discount(p.pomdp) * sum(length(D.scenarios[bp]) * D.U[bp] for bp in D.ba_children[ba]) for ba in D.children[b]) / length(D.scenarios[b])
-        D.mu[b] = max(D.l_0[b], maximum(D.ba_mu[ba] for ba in D.children[b]))
-        D.mu[b] = max(D.l_0[b], maximum(D.ba_l[ba] for ba in D.children[b]))
+        #=
+        max_mu = maximum(D.ba_rho[ba] + sum(D.mu[bp] for bp in D.ba_children[ba]) for ba in D.children[b])
+        max_l = maximum(D.ba_rho[ba] + sum(D.l[bp] for bp in D.ba_children[ba]) for ba in D.children[b])
+        =#
+        max_U = -Inf
+        max_mu = -Inf
+        max_l = -Inf
+        for ba in D.children[b]
+            weighted_sum_U = 0.0
+            sum_mu = 0.0
+            sum_l = 0.0
+            for bp in D.ba_children[ba]
+                weighted_sum_U += length(D.scenarios[bp]) * D.U[bp]
+                sum_mu += D.mu[bp]
+                sum_l += D.l[bp]
+            end
+            new_U = (D.ba_Rsum[ba] + discount(p.pomdp) * weighted_sum_U)/length(D.scenarios[b])
+            new_mu = D.ba_rho[ba] + sum_mu
+            new_l = D.ba_rho[ba] + sum_l
+            max_U = max(max_U, new_U)
+            max_mu = max(max_mu, new_mu)
+            max_l = max(max_l, new_l)
+        end
+
+        l_0 = D.l_0[b]
+        D.U[b] = max_U
+        D.mu[b] = max(l_0, max_mu)
+        D.l[b] = max(l_0, max_l)
     end
-    return nothing
 end
 
 function excess_uncertainty(D::DESPOT, b::Int, p::PL_DESPOTPlanner)
